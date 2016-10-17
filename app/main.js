c293f29dcec63830e97dd3417430ad1c5fec4677
RayChen <raychen@Ray-Chens-MacBook-Pro.local>
2016-10-17 08:28:25 +0800

    init

---
+++ app/main.js
@@ -0,0 +1,6 @@
+//main.js 
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+render(<Greeter />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/**
+ * Created by raychen on 2016/10/16.
+ */
+module.exports = {
+    devtool: 'eval-source-map',
+    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+    output: {
+        path: __dirname + "/public",//打包后的文件存放的地方
+        filename: "bundle.js"//打包后输出文件的文件名
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: "json"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        colors: true,//终端中输出结果为彩色
+        historyApiFallback: true,//不跳转
+        inline: true,//实时刷新
+        port: 4004
+    }
+}
