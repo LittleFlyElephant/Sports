6d187de679fe4d4f47eebd98f3f7e8169f2b54c7
RayChen <raychen@Ray-Chens-MacBook-Pro.local>
2016-10-18 21:55:32 +0800

    login

--- app/components/LoginCard/LoginCard.js
+++ app/components/LoginCard/LoginCard.js
@@ -6,6 +6,7 @@ import React, {Component} from 'react'
 
 import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import './LoginCard.scss'
 
@@ -15,8 +16,26 @@ class LoginCard extends Component{
         return (
             <div className="container">
                 <Paper className="card" zDepth={2}>
-                    <TextField hintText="username" />
-                    <TextField hintText="password" type="password" />
+                    <TextField
+                        hintText="username"
+                        fullWidth={true}
+                    />
+                    <TextField
+                        hintText="password"
+                        fullWidth={true}
+                        type="password"
+                    />
+                    <RaisedButton
+                        fullWidth={true}
+                        label="login"
+                        primary={true}
+                        className="button"
+                    />
+                    <RaisedButton
+                        fullWidth={true}
+                        label="sign up"
+                        className="button"
+                    />
                 </Paper>
             </div>
         );
